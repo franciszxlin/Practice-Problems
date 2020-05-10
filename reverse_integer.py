f7d644f524bb91155caf644b20329e56cf200cc5
Francis Lin <franciszxlin@gmail.com>
2020-05-10 19:34:57 -0400

    Create reverse_integer.py

---
+++ reverse_integer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 19:20:49 2020
+
+@author: Francis Lin
+"""
+
+"""
+Given a 32-bit signed integer, reverse digits of an integer.
+"""
+def reverse(x):
+    negative = False
+    if x < 0: negative, x = True, -x
+    ans = 0
+    while x > 0:
+        ans = ans * 10 + x % 10
+        x //= 10
+    if(ans > (2 ** 31 - 1)): return 0
+    return -ans if negative else ans
+
+def main():
+    original_integer = 8464200
+    reversed_integer = reverse(original_integer)
+    print('Output : {}'.format(reversed_integer))
+
+if __name__ == '__main__': 
+    main()
