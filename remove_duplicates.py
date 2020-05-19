c66e2212eff030efadbef7f91fe020e3c9a2cac1
Francis Lin <franciszxlin@gmail.com>
2020-05-18 23:24:24 -0400

    Create remove_duplicates.py

---
+++ remove_duplicates.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 23:16:27 2020
+
+@author: Francis Lin 
+"""
+
+"""
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+"""
+
+def remove_duplicates(array):
+    # The first element indexed at 0 is always unique (non-unique).
+    index = 1
+    for i in range(len(array) - 1): 
+        if array[i] != array[i + 1]: 
+            array[index] =array[i + 1]
+            index += 1
+    return index
+
+def main(): 
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    ans = remove_duplicates(nums)
+    print('Output : {}'.format(ans)) # Expect 5. 
+
+if __name__ == '__main__': 
+    main()
+
+
