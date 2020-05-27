f7082550967c20055e49bee64a1faee0f8107844
Francis Lin <franciszxlin@gmail.com>
2020-05-26 23:15:00 -0400

    Update maximum_sub_array.py

--- maximum_sub_array.py
+++ maximum_sub_array.py
@@ -17,22 +17,17 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 
 # Solution utilizes Kadane's algorithm.
 def max_sub_array(nums): 
-    max_ending_here, max_so_far, start, end = 0, 0, 0, 0 
-    for i, num in enumerate(nums): 
-        max_ending_here += num
-        if max_ending_here < 0: 
-            max_ending_here = 0
-            start = i + 1
-        if max_so_far < max_ending_here: 
-            max_so_far = max_ending_here
-            end = i
-    return max_so_far, start, end
+    max_ending_here, max_so_far = nums[0], nums[0] 
+    for num in nums[1:]:  
+        max_ending_here = max(max_ending_here + num, num)
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
 
 def main():
-    array = [-2, -3, 4, -1, -2, 1, 5, -3] # Expect to see 7, 2, 6
-    #array = [-2, 1, -3, 4, -1, 2, 1, -5, 4] # Expect to see 6, 3, 6
-    ans, start, end = max_sub_array(array)
-    print('Output : {} from {} to {}'.format(ans, start, end))
+    array = [-2, -3, 4, -1, -2, 1, 5, -3] # Expect to see 7
+    #array = [-2, 1, -3, 4, -1, 2, 1, -5, 4] # Expect to see 6
+    ans = max_sub_array(array)
+    print('Output : {}'.format(ans))
 
 if __name__ == '__main__': 
     main()
